cf3c195dc83c8a185499a645222a7c53b9ebd0b4
Kelly Koome <koomekelly7@gmail.com>
2025-03-29 15:07:07 +0300

    done

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Return the fetch call to allow promise chaining
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json()) // Invoke json() to parse the response
+    .then(json => renderBooks(json));  // Pass the parsed JSON data to renderBooks
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
